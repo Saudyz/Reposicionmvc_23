ca518bab69048705c3173c13af249679989b3e33
saudyz <saudyzavala123@gmail.com>
2023-04-28 08:24:28 -0600

    Corregir variables de la base de datos
    corrección

--- docs/scripts/11_Carros.sql
+++ docs/scripts/11_Carros.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `nw202301`.`Carros` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `bin` INT NULL,
+  `bin` VARCHAR(45),
   `placaCarro` VARCHAR(45) NULL,
   `modeloCarro` VARCHAR(45) NULL,
-  `anoCarro` VARCHAR(45) NULL,
+  `anoCarro` int Default NULL,
   PRIMARY KEY (`id`));
